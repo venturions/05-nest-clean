7a39204ff65f2ba265ec4a8e4921fea9b40f0589
venturions <alex.eduardo10@hotmail.com>
2025-05-01 18:37:17 -0300

    Configurando Vitest com SWC

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -7,9 +7,9 @@ import {
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { compare } from 'bcryptjs'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { z } from 'zod'
+import { PrismaService } from '@/prisma/prisma.service'
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Post, UseGuards, UsePipes } from '@nestjs/common'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
 import { z } from 'zod'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { PrismaService } from 'src/prisma/prisma.service'
-import { CurrentUser } from 'src/auth/current-user-decorator'
-import { UserPayload } from 'src/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
+import { CurrentUser } from '@/auth/current-user-decorator'
+import { UserPayload } from '@/auth/jwt.strategy'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
