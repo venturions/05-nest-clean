cec13d3bac6dad1fbc11c33c525ebfefca0bb66c
venturions <alex.eduardo10@hotmail.com>
2025-05-01 11:28:09 -0300

    Criando Pipe de validação do Zod

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -4,17 +4,28 @@ import {
   HttpCode,
   Post,
   Body,
+  UsePipes,
 } from '@nestjs/common'
 import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
   @Post()
   @HttpCode(201)
-  async handle(@Body() body: any) {
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSameEmail = await this.prisma.user.findUnique({
---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,27 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    try {
+      const parsedValue = this.schema.parse(value)
+      return parsedValue
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          statusCode: 400,
+          message: 'Validation failed',
+          errors: fromZodError(error),
+        })
+      }
+      throw new BadRequestException('Validation failed')
+    }
+  }
+}
