c53f752c2d68cab1a1d0b8bc83e1ae0e33e58054
venturions <alex.eduardo10@hotmail.com>
2025-05-02 12:13:31 -0300

    Copiando camada de Domínio

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
   resolve: {
     alias: {
       // Ensure Vitest correctly resolves TypeScript path aliases
+      '@': resolve(__dirname, './src'),
       src: resolve(__dirname, './src'),
     },
   },
