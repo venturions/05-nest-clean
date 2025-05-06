14295030e69c24539c0ff28964aa40d827d30d11
venturions <alex.eduardo10@hotmail.com>
2025-05-06 14:18:55 -0300

    Criando EnvModule

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core'
-import { ConfigService } from '@nestjs/config'
-import { Env } from './infra/env'
 import { AppModule } from './infra/app.module'
+import { EnvService } from './infra/env/env.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    //  { logger: false }
-  )
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  })
 
-  const configService: ConfigService<Env, true> = app.get(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+  const configService = app.get(EnvService)
+  const port = configService.get('PORT')
+
   await app.listen(port)
 }
+
 bootstrap()
