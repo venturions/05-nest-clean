73da85c121085dba05fdaff4f20e41eb26485a7f
venturions <alex.eduardo10@hotmail.com>
2025-05-07 20:35:12 -0300

    Controller comentário com autor

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -8,7 +8,7 @@ import {
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 import { z } from 'zod'
 import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
-import { CommentPresenter } from '../presenters/comment-presenter'
+import { CommentWithAuthorPresenter } from '../presenters/comment-with-author-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -38,9 +38,8 @@ export class FetchQuestionCommentsController {
     if (result.isLeft()) {
       throw new BadRequestException()
     }
+    const comments = result.value.comments
 
-    const questionComments = result.value.questionComments
-
-    return { comments: questionComments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
