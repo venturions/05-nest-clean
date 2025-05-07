6c358a70b7d56fad6550e7f0be7eb83a70575e2f
venturions <alex.eduardo10@hotmail.com>
2025-05-07 12:40:27 -0300

    Testando controller de upload

--- src/domain/forum/enterprise/entities/attachment.ts
+++ src/domain/forum/enterprise/entities/attachment.ts
@@ -11,7 +11,7 @@ export class Attachment extends Entity<AttachmentProps> {
     return this.props.title
   }
 
-  get link() {
+  get url() {
     return this.props.url
   }
 
--- src/infra/http/controllers/upload-attachment.controller.ts
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -1,3 +1,5 @@
+import { InvalidAttachmentType } from '@/domain/forum/application/use-cases/errors/invalid-attachment-type'
+import { UploadAndCreateAttachmentUseCase } from '@/domain/forum/application/use-cases/upload-and-create-attachment'
 import {
   Controller,
   Post,
@@ -6,12 +8,15 @@ import {
   ParseFilePipe,
   MaxFileSizeValidator,
   FileTypeValidator,
+  BadRequestException,
 } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 
 @Controller('/attachments')
 export class UploadAttachmentController {
-  //   constructor() {}
+  constructor(
+    private uploadAndCreateAttachment: UploadAndCreateAttachmentUseCase,
+  ) {}
 
   @Post()
   @UseInterceptors(FileInterceptor('file'))
@@ -26,6 +31,27 @@ export class UploadAttachmentController {
     )
     file: Express.Multer.File,
   ) {
-    console.log(file)
+    const result = await this.uploadAndCreateAttachment.execute({
+      fileName: file.originalname,
+      fileType: file.mimetype,
+      body: file.buffer,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case InvalidAttachmentType:
+          throw new BadRequestException(error.message)
+        default:
+          throw new BadRequestException(error.message)
+      }
+    }
+
+    const { attachment } = result.value
+
+    return {
+      attachmentId: attachment.id.toString(),
+    }
   }
 }
