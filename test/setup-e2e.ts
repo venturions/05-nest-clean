23324df9da9866c5845874d0cde768b924b504cb
venturions <alex.eduardo10@hotmail.com>
2025-05-08 12:03:21 -0300

    Testando Persistencia em Cache

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,21 +1,30 @@
 import { config } from 'dotenv'
 
 import { PrismaClient } from '@prisma/client'
-import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { DomainEvents } from '@/core/events/domain-events'
+import { Redis } from 'ioredis'
+import { envSchema } from '@/infra/env/env'
 
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not set')
+  if (!env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
 
@@ -30,6 +39,8 @@ beforeAll(async () => {
   process.env.DATABASE_URL = databaseURL
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   execSync('npx prisma migrate deploy')
 })
